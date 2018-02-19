ccd528ee5ae92f97fa63933a47460e695605f8b4
jason0597 <jason099080@hotmail.com>
2018-02-19 19:04:28 +0000

    a bit more cleanup

--- src/MainPkg/Seedplanter.java
+++ src/MainPkg/Seedplanter.java
@@ -1,13 +1,11 @@
 package MainPkg;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Seedplanter {
-    private Data data = null;
-    private String pDSiWareStr;
+    private Data data;
 
     //The constructor will read in all of the files into byte arrays
     Seedplanter(String DSiWareStr, String movableSedStr, String injectionZipStr, String ctcertStr) throws IOException {
@@ -20,15 +18,12 @@ public class Seedplanter {
         if (ctcertStr == null || Files.notExists(Paths.get(ctcertStr)))
             throw new IOException("ctcert.bin not found!");
 
-        pDSiWareStr = DSiWareStr;
-
         data = new Data(DSiWareStr, movableSedStr, injectionZipStr);
     }
 
     public void DoInjection() throws IOException {
         /*
             DSiWare is in RAM, movable.sed is in RAM, the (extracted) stuff from the injection ZIP is in RAM
-
             1) Decrypt the DSiWare. Theoretically there should now be a multi-dimensional array returned with all the decrypted data
                 Maybe instead of returning a multi-dimensional array, make it slot everything into the Data POD class?
             2) Now that the decrypted data is in RAM, we can call arraycopy to inject the srl.nds, and we can use the FAT code to inject savedata
@@ -37,7 +32,6 @@ public class Seedplanter {
             5) Re-import footer.bin into RAM
             6) Encrypt the DSiWare
             7) Export the encrypted DSiWare to desktop
-
             Done!
          */
     }
