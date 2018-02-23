233089976c157204b7c80a1471d4cf6538594382
jason0597 <jason099080@hotmail.com>
2018-02-23 17:19:10 +0000

    Move to maven part 3

---
+++ src/main/java/DataHandling.java
@@ -0,0 +1,70 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
+import java.util.zip.ZipFile;
+
+public class DataHandling {
+    private ZipHandling.ZipRegion ZIPR;
+    private Path tmpDir;
+    public HashMap<String, byte[]> MainData = new HashMap<>();
+
+    public ZipHandling.ZipRegion getZipRegion()    { return ZIPR; }
+    public Path      getTmpdirPath()               { return tmpDir; }
+
+    DataHandling(String DSiWareStr, String movableSedStr, String injectionZipStr, String ctcertStr) throws IOException {
+        try (ZipFile zf = new ZipFile(injectionZipStr)) {
+            //===============================================================
+            //========Read in DSiWare, movable.sed, and ZIP entries==========
+            //===============================================================
+            ZIPR = ZipHandling.CheckRegion(Files.readAllBytes(Paths.get(injectionZipStr)));
+            if (ZIPR == ZipHandling.ZipRegion.ZIP_ERROR)
+                throw new IOException("ZIP Region Error");
+
+            if (ZIPR == ZipHandling.ZipRegion.ZIP_EUR || ZIPR == ZipHandling.ZipRegion.ZIP_USA)
+                readFilesEURUSA(zf);
+            else if (ZIPR == ZipHandling.ZipRegion.ZIP_JPN)
+                throw new IOException("JPN region is not supported yet!");
+
+            readMovableSed(movableSedStr);
+            readDSiWare(DSiWareStr);
+
+            //===============================================================
+            //==========Copy dsiwaretool and DLLs/ctcert to tmpDir===========
+            //===============================================================
+            tmpDir = Files.createTempDirectory("Seedplanter");
+
+            Files.copy(getClass().getResourceAsStream("ctr-dsiwaretool.exe"), Paths.get(tmpDir.toString(), "ctr-dsiwaretool.exe"));
+            Files.copy(getClass().getResourceAsStream("libcrypto-1_1-x64__.dll"), Paths.get(tmpDir.toString(), "libcrypto-1_1-x64__.dll"));
+            Files.copy(getClass().getResourceAsStream("zlib1__.dll"), Paths.get(tmpDir.toString(), "zlib1__.dll"));
+
+            Files.copy(Paths.get(ctcertStr), Paths.get(tmpDir.toString(), "ctcert.bin"));
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+    private void readFilesEURUSA(ZipFile zf) throws IOException {
+        MainData.put("sudoku_v0.app", ZipHandling.ReadAllBytesFromZipEntry(zf, "sudoku_v0.app"));
+        MainData.put("savedata.bin", ZipHandling.ReadAllBytesFromZipEntry(zf, "savedata/savedata.bin"));
+    }
+
+    private void readMovableSed(String movableSedStr) throws IOException {
+        MainData.put("movable.sed", new byte[16]);
+        System.arraycopy(Files.readAllBytes(Paths.get(movableSedStr)), 0x110, MainData.get("movable.sed"), 0, 16);
+    }
+
+    private void readDSiWare(String DSiWareStr) throws IOException {
+        MainData.put("dsiware.bin", Files.readAllBytes(Paths.get(DSiWareStr)));
+    }
+
+    public void exportToFile(byte[] filedata, Path filename) throws IOException {
+        Files.write(filename, filedata, StandardOpenOption.CREATE);
+    }
+
+    public byte[] importToByteArray(Path filename) throws IOException {
+        return Files.readAllBytes(filename);
+    }
+}
---
+++ src/main/java/Seedplanter.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Seedplanter {
+    private DataHandling Data;
+
+    //The constructor will read in all of the files into byte arrays
+    Seedplanter(String DSiWareStr, String movableSedStr, String injectionZipStr, String ctcertStr) throws IOException {
+        if (DSiWareStr == null || Files.notExists(Paths.get(DSiWareStr)))
+            throw new IOException("DSiWare not found!");
+        if (movableSedStr == null || Files.notExists(Paths.get(movableSedStr)))
+            throw new IOException("movable.sed not found!");
+        if (injectionZipStr == null || Files.notExists(Paths.get(injectionZipStr)))
+            throw new IOException("Injection ZIP not found!");
+        if (ctcertStr == null || Files.notExists(Paths.get(ctcertStr)))
+            throw new IOException("ctcert.bin not found!");
+
+        Data = new DataHandling(DSiWareStr, movableSedStr, injectionZipStr, ctcertStr);
+    }
+
+    public void DoInjection() throws IOException {
+        /*
+        DSiWare is in RAM, movable.sed is in RAM, the (extracted) stuff from the injection ZIP is in RAM. now what?
+
+        1) Decrypt the DSiWare. Theoretically there should now be a multi-dimensional array returned with all the decrypted data
+            Maybe instead of returning a multi-dimensional array, make it slot everything into the Data POD class?
+            Maybe instead of working with raw arrays, use a Map object?
+        2) Now that the decrypted data is in RAM, we can call arraycopy to inject the srl.nds, and we can use the FAT code to inject savedata
+        */
+
+        byte[] footer_array = {1}; //placeholder array
+        String tmpDirPath = Data.getTmpdirPath().toString();
+        Path footerPath = Paths.get(tmpDirPath, "/footer.bin");
+        Data.exportToFile(footer_array, footerPath);
+
+        Runtime.getRuntime().exec(
+                tmpDirPath + "/ctr-dsiwaretool.exe " + tmpDirPath + "/footer.bin " + tmpDirPath + "/ctcert.bin --write"
+        );
+
+        footer_array = Data.importToByteArray(footerPath);
+
+        /*
+        6) Encrypt the DSiWare
+        7) Export the encrypted DSiWare to desktop
+             the cherry on top of the cake
+
+         Done!
+         */
+    }
+}
