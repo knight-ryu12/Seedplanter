2eabea4bdb6d1296358249f1b7033925a37fb1c0
Iason Papadopoulos <jason099080@hotmail.com>
2018-03-04 18:21:46 +0000

    Remove multi-platform process creation
    Trying it out locally, multi-platform process creation with ProcessBuilder is an absolute pain, you don't want to even bother
    
    It seems like we really have to rewrite ctr-dsiwaretool in Java, oh well.
    
    This commit removes all multi-platform attempts and will simply throw a short and sweet exception if you are not running Windows 64 bit
    
    Oh, and I added some code to determine if decryption went smooth or not, seems like some people use the wrong movable.sed for their DSiWare..........

--- src/main/java/faith/elguadia/seedplanter/Signing.java
+++ src/main/java/faith/elguadia/seedplanter/Signing.java
@@ -2,9 +2,7 @@ package faith.elguadia.seedplanter;
 
 import org.apache.commons.lang3.SystemUtils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,60 +12,42 @@ public class Signing {
     private String tmpDirStr;
 
     Signing() throws IOException {
+        if (!SystemUtils.IS_OS_WINDOWS || System.getenv("ProgramFiles(x86)") == null)
+            throw new IOException("You are not running Windows 64 bit!");
+
         tmpDirStr = Files.createTempDirectory("Seedplanter").toString();
     }
 
     public void ExportFiles(byte[] footer, Path ctcert) throws IOException {
-        if (SystemUtils.IS_OS_WINDOWS) {
-            Files.copy(getClass().getClassLoader().getResourceAsStream("ctr-dsiwaretool.exe"), Paths.get(tmpDirStr, "ctr-dsiwaretool.exe"));
-            Files.copy(getClass().getClassLoader().getResourceAsStream("libcrypto-1_1-x64__.dll"), Paths.get(tmpDirStr, "libcrypto-1_1-x64__.dll"));
-            Files.copy(getClass().getClassLoader().getResourceAsStream("zlib1__.dll"), Paths.get(tmpDirStr, "zlib1__.dll"));
-        } else if (SystemUtils.IS_OS_MAC) {
-            Files.copy(getClass().getClassLoader().getResourceAsStream("mac_ctr-dsiwaretool"), Paths.get(tmpDirStr, "mac_ctr-dsiwaretool"));
-        } else if (SystemUtils.IS_OS_LINUX) {
-            Files.copy(getClass().getClassLoader().getResourceAsStream("linux_ctr-dsiwaretool"), Paths.get(tmpDirStr, "linux_ctr-dsiwaretool"));
-        }
+        Files.copy(getClass().getClassLoader().getResourceAsStream("ctr-dsiwaretool.exe"), Paths.get(tmpDirStr, "ctr-dsiwaretool.exe"));
+        Files.copy(getClass().getClassLoader().getResourceAsStream("libcrypto-1_1-x64__.dll"), Paths.get(tmpDirStr, "libcrypto-1_1-x64__.dll"));
+        Files.copy(getClass().getClassLoader().getResourceAsStream("zlib1__.dll"), Paths.get(tmpDirStr, "zlib1__.dll"));
 
         Files.write(Paths.get(tmpDirStr, "footer.bin"), footer, StandardOpenOption.CREATE);
         Files.copy(ctcert, Paths.get(tmpDirStr, "ctcert.bin"));
     }
 
     public void DoSigning() throws IOException, InterruptedException {
-        String command = null;
+        String[] command = {
+                tmpDirStr + "\\ctr-dsiwaretool.exe", tmpDirStr + "\\footer.bin", tmpDirStr + "\\ctcert.bin" , "--write"
+        };
 
-        if (SystemUtils.IS_OS_WINDOWS) {
-            System.out.println("============== DETECTED WINDOWS ==============");
-            command = tmpDirStr + "\\ctr-dsiwaretool.exe " + tmpDirStr + "\\footer.bin " + tmpDirStr + "\\ctcert.bin " + "--write";
-        } else if (SystemUtils.IS_OS_MAC) {
-            System.out.println("============== DETECTED MAC ==============");
-            command = "." + tmpDirStr + "/mac_ctr-dsiwaretool "   + tmpDirStr + "/footer.bin " + tmpDirStr + "/ctcert.bin " + "--write";
-        } else if (SystemUtils.IS_OS_LINUX) {
-            System.out.println("============== DETECTED LINUX ==============");
-            command = "." + tmpDirStr + "/linux_ctr-dsiwaretool " + tmpDirStr + "/footer.bin " + tmpDirStr + "/ctcert.bin " + "--write";
-        }
-
+        System.out.println("Executing command --> " + String.join(" ", command));
         System.out.println("============== EXECUTING CTR-DSIWARETOOL ==============");
-        System.out.println("Executing command --> " + command);
-        Process proc = Runtime.getRuntime().exec(command);
+
+        ProcessBuilder procb = new ProcessBuilder(command);
+        procb.inheritIO();
+        Process proc = procb.start();
 
         proc.waitFor();
-        final int exitValue = proc.waitFor();
+        int exitValue = proc.waitFor();
 
-        System.out.println("============== OUTPUT FROM CTR-DSIWARETOOL ==============");
-        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-        BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
-        String str;
-        while ((str = stdInput.readLine()) != null)
-            System.out.println(str);
-        // read any errors from the attempted command
-        while ((str = stdError.readLine()) != null)
-            System.out.println(str);
         System.out.println("============== END OF CTR-DSIWARETOOL ==============");
 
         if (exitValue == 0)
-            System.out.println("Successfully executed the command");
+            System.out.println("======== Successfully executed the command ========");
         else {
-            System.out.println("Failed to execute the command");
+            System.out.println("======== Failed to execute the command ========");
             throw new IOException("Failed to sign the footer!");
         }
     }
